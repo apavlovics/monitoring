1deb09185954c41b3d703ff89d65740e6f6726a3
Andrejs Pavlovics <andrey.pavlovich@me.com>
2020-08-13 22:27:32 +0300

    Rename and optimize main class

---
+++ src/main/java/lv/continuum/monitoring/Monitoring.java
@@ -0,0 +1,12 @@
+package lv.continuum.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Monitoring {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Monitoring.class, args);
+    }
+}
